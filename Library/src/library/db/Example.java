0411488a87461eb9ffc8733d2d81fc76fd0c00a1
gzomo <gzomo@10.60.58.246>
2024-03-14 18:25:30 +0100

    Initial commit

---
+++ Library/src/library/db/Example.java
@@ -0,0 +1,11 @@
+package library.db;
+
+public class Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola");
+		System.out.print("HOLA");
+	}
+
+}
